74184d8e3b872ccd338a02328a7acb4901dff43e
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-05-20 16:35:49 -0400

    completed day list UI from  left panel

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const dayClass = "day-list__item" + (
+    props.selected ? "--selected" : 
+    (props.spots === 0 ? "--full" : "")
+  );
+
+  const formatSpots = () => {
+    if(props.spots === 1) {
+      return `1 spot remaining`;
+    } else if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    } else {
+      return `no spots remaining`;
+    }
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
